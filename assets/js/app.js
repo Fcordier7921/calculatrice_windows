ebe0fde032f144cf0ff1fd5349a8426290f10248
Fcordier7921 <fredericcordierpro6@gmail.com>
2021-10-31 13:08:09 +0100

    fin animation de l'ouverture et fermeture de la fenetre

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,27 +5,38 @@ const iconRedutionFenetre = document.querySelector('.moin');
 const iconAgrandirFenetre = document.querySelector('.Agrendir');
 const iconFermerFenetre = document.querySelector('.close');
 
-
-
-
-iconCalculatrice.addEventListener('click', () => {
+function CacheContainer() {
     if (containerCalculatrice.classList.contains('containerCache')) {
         containerCalculatrice.classList.remove('containerCache');
     } else {
         containerCalculatrice.classList.add('containerCache');
     }
+}
+
+
+iconCalculatrice.addEventListener('click', () => {
+    CacheContainer();
 })
 iconRedutionFenetre.addEventListener('click', () => {
     if (containerCalculatrice.classList.contains('containerReduit')) {
         containerCalculatrice.classList.remove('containerReduit');
+        containerCalculatrice.style.animation = 'removrReduire 1s forwards';
     } else {
         containerCalculatrice.classList.add('containerReduit');
+
     }
 
 })
 iconAgrandirFenetre.addEventListener('click', () => {
+    containerCalculatrice.classList.remove('containerReduit');
+    if (containerCalculatrice.classList.contains('containerAgrandis')) {
+        containerCalculatrice.classList.remove('containerAgrandis');
 
+    } else {
+        containerCalculatrice.classList.add('containerAgrandis');
+
+    }
 })
 iconFermerFenetre.addEventListener('click', () => {
-
+    CacheContainer();
 })
